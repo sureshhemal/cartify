3f53e06b514b513ecd92e589612beb41ce7321c4
sureshhemal <sureshhemal123@gmail.com>
2024-09-24 21:03:41 +0530

    Configure axios

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,6 @@
 import axios from 'axios'
-window.axios = axios
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+window.$http = axios
+
+window.$http.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+window.$http.defaults.baseURL = 'api/'
